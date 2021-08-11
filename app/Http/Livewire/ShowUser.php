9cd04fd4708cccf231bc274229465556a524c859
dedelukman <dlukmanul.h@gmail.com>
2021-08-11 16:37:34 +0700

    implement datatable in show user

--- app/Http/Livewire/ShowUser.php
+++ app/Http/Livewire/ShowUser.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Branch;
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\DB;
 
 class ShowUser extends Component
 {
+    use WithPagination;
+
+
+    public $search;
+
+    public $sortField;
+
+    
+
+
     public function render()
     {
-        return view('livewire.show-user');
+        return view('livewire.show-user', [
+            // 'users'=>User::where('name','like', "%{$this->search}%")->paginate(10)
+        'users' => DB::table('users')
+        ->join('branches', 'users.branch_id','branches.id')
+        ->select('users.*', 'branches.name as nameBranch')
+        ->where('users.name','like',"%{$this->search}%")
+        ->orWhere('users.email','like',"%{$this->search}%")
+        ->orWhere('branches.name','like',"%{$this->search}%")
+        ->latest()->paginate(10)
+        ]);
     }
 }
--- resources/views/livewire/show-user.blade.php
+++ resources/views/livewire/show-user.blade.php
@@ -1,12 +1,46 @@
 <div>
+<div class="py-4">
+    <div class="flex justify-between">
+                <div class=" mb-3 w-1/4">
+                    <x-input.text wire:model="search" placeholder="Search User..." 
+                    class="py-1 px-2 rounded-lg w-1/4"/>
+                </div>
+            </div>
+<div class="flex-col space-y-4 table-responsive">
     <x-table>
         <x-slot name="head">
-            <x-table.heading>Hey</x-table.heading>
-            <x-table.heading>Hey</x-table.heading>
+            <x-table.heading sortable direction="desc">Name</x-table.heading>
+            <x-table.heading sortable>Email</x-table.heading>
+            <x-table.heading sortable>Bagian</x-table.heading>
+            <x-table.heading sortable>Akses</x-table.heading>
+            <x-table.heading sortable>Status</x-table.heading>
         </x-slot>
         <x-slot name="body">
-            <x-table.heading>Hey</x-table.heading>
-            <x-table.heading>Hey</x-table.heading>
+            @forelse ( $users as $user )
+                <x-table.row>
+                    <x-table.cell>{{ $user->name }}</x-table.cell>
+                    <x-table.cell>{{ $user->email }}</x-table.cell>
+                    <x-table.cell>{{ $user->nameBranch}}</x-table.cell>
+                    <x-table.cell>{{ $user->role }}</x-table.cell>
+                    <x-table.cell>{{ $user->active }}</x-table.cell>
+                </x-table.row>
+            @empty
+                <x-table.row>
+                        <x-table.cell colspan="5">
+                            <div class="flex justify-center items-center space-x-2">
+                                <x-icon.inbox class="h-8 w-8 text-cool-gray-400" />
+                                <span class="font-medium py-8 text-cool-gray-400 text-xl">Data yang ada cari tidak ada...</span>
+                            </div>
+                        </x-table.cell>
+                    </x-table.row>
+            @endforelse 
+    
+           
+            
         </x-slot>
     </x-table>
+    <div class="mt-3"> {{ $users->links() }}</div>
+</div>
+
+</div>
 </div>
