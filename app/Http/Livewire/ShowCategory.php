4dcb5747cada912ed8682b69ffffd636d569fa7a
dedelukman <dlukmanul.h@gmail.com>
2021-08-12 17:43:08 +0700

    add crud showCategory

--- app/Http/Livewire/ShowCategory.php
+++ app/Http/Livewire/ShowCategory.php
@@ -3,11 +3,106 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Category as Entities;
 
 class ShowCategory extends Component
 {
+    use WithPagination;
+
+
+    public $search;
+    public $sortField='name';
+    public $sortDirection ='desc';    
+    public $titleEditModal = 'Edit';
+    public Entities $editing;
+    public Entities $deleting;
+    
+
+    
+
+    public function rules() { 
+        return [
+            'editing.code' => 'required|min:2',
+            'editing.name' => 'required',
+            'editing.photo' => 'required',            
+        ]; 
+    }
+
+    public function makeBlankTransaction(){
+        return Entities::make();
+    }
+
+    public function create(){
+        $this->editing =  $this->makeBlankTransaction();
+        $this->titleEditModal='Tambah';
+    }
+
+    public function edit(Entities $entity){
+        $this->editing = $entity;
+         $this->titleEditModal='Edit';
+    }
+
+    public function deleteId(Entities $entity)
+    {
+        $this->deleting = $entity;
+    }
+
+    public function delete(){
+        $this->deleting->delete();
+        $this->dispatchBrowserEvent('alert',[
+                'type'=>'success',
+                'message'=>"Data Berhasil di Delete!!"
+            ]);
+    }
+
+    public function sortBy($field){
+
+        if($this->sortField == $field){
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        }else{
+            $this->sortDirection = 'asc';
+        }
+        $this->sortField = $field;
+    }
+
+     public function changeRole(Entities $entity, $akses){
+        $entity->role = $akses;
+        $entity->save();
+      
+    }
+
+    public function changeActive(Entities $entity, $akses){
+        $entity->active = $akses;
+        $entity->save();
+        
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        try {
+            $this->editing->save();    
+            $this->dispatchBrowserEvent('alert',[
+                'type'=>'success',
+                'message'=>"Data Berhasil Disimpan!!"
+            ]);
+        } catch (\Exception $e) {
+            $this->dispatchBrowserEvent('alert',[
+                'type'=>'error',
+                'message'=>"Data Tidak Berhasil Disimpan!!"
+            ]);
+        }
+    }
+
     public function render()
     {
-        return view('livewire.show-category');
+        return view('livewire.show-category',
+        ['entities' => Entities::
+        where('code','like',"%{$this->search}%")
+        ->orWhere('name','like',"%{$this->search}%")       
+        ->orderBy($this->sortField, $this->sortDirection)
+        ->paginate(10)]);
     }
 }
--- resources/views/livewire/show-category.blade.php
+++ resources/views/livewire/show-category.blade.php
@@ -1,3 +1,169 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    
+    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">Daftar Category</h1>
+                    <p class="mb-4">Daftar Kategori produk claresta.</p>
+
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Daftar Data</h6>
+                        </div>
+                        <div class="card-body">
+                            <!-- Topbar Search -->
+                            <div class="flex flex-auto">
+                                <form
+                                class="d-sm-inline-block form-inline mr-auto ml-md-3 mw-100 mb-3 navbar-search">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                                            aria-label="Search" aria-describedby="basic-addon2" wire:model="search">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-primary" type="button">
+                                                <i class="fas fa-search fa-sm"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form> 
+                                <div >
+                                    <button class="btn btn-primary " data-toggle="modal" data-target="#formModal" wire:click="create">
+                                        <i class="fas fa-plus fa-sm"> New</i>
+                                    </button>  
+                                </div>
+                            </div>
+                            
+                                                 
+                            <div class="table-responsive">
+                                <table class="table table-bordered" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th wire:click="sortBy('code')" ><i class="fa fa-fw fa-sort"></i>Kode</th>
+                                            <th wire:click="sortBy('name')" ><i class="fa fa-fw fa-sort"></i>Name</th>
+                                            {{-- <th wire:click="sortBy('photo')" ><i class="fa fa-fw fa-sort"></i>Photo</th>                                                                                     --}}
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                   
+                                    <tbody>
+                                        @forelse($entities as $entity )
+                                            <tr wire:loading.class.delay="opacity-50">
+                                                <td>{{ $entity->code }}</td>
+                                                <td>{{ $entity->name }}</td>
+                                                {{-- <td>{{ $entity->photo }}</td>                                                                                              --}}
+                                                <td>                                                                                                                                                       
+                                                    <div class="dropdown mb-4">
+                                                        <button class="dropdown-toggle" type="button"
+                                                            id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                                            aria-expanded="false">
+                                                            Aksi
+                                                        </button>
+                                                        <div class="dropdown-menu animated--fade-in"
+                                                            aria-labelledby="dropdownMenuButton">
+                                                            <a class="dropdown-item" 
+                                                            wire:click="edit({{ $entity->id }})"
+                                                            data-toggle="modal" data-target="#formModal"
+                                                            >Edit</a>
+                                                            <a class="dropdown-item" data-toggle="modal" data-target="#deleteModal"
+                                                            wire:click="deleteId({{ $entity->id }})"
+                                                            >Delete</a>                                                        
+                                                        </div>
+                                                    </div>
+                                                    
+                                                </td>                                                
+                                            </tr>
+                                        
+                                            @empty
+
+                                            <tr>
+                                                <td colspan="3" style="text-align: center;"> 
+                                                    <p class="py-3">
+                                                        <i class="fas fa-archive fa-sm"></i> Data yang Anda cari tidak ditemukan
+                                                    </p>
+                                                   
+                                                </td>
+                                            </tr>
+                                            
+                                        @endforelse 
+                                       
+                                       
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div class="mt-4">{{ $entities->links() }}</div>
+                        </div>
+                    </div>
+
+        <!-- Modal Form-->
+        <form action="">            
+            <div wire:ignore.self class="modal fade" id="formModal"
+             tabindex="-1" role="dialog" aria-labelledby="formModalLabel" aria-hidden="true">
+                <div class="modal-dialog" wire:loading.class.delay="opacity-50">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="formModalLabel">
+                            {{ $titleEditModal }}
+                             Data</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="code">Kode</label>
+                            <input type="text" class="form-control" id="code" aria-describedby="code"
+                            wire:model.defer="editing.code" :error="$errors->first('editing.code')" required
+                            >
+                          
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Nama</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="name"
+                            wire:model.defer="editing.name" required
+                            >
+                           
+                        </div>
+                        
+                        {{-- FOTO NEXT --}}
+                        
+                                                                  
+                       
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary"
+                        wire:click.prevent="save()" data-dismiss="modal" required
+                        >Save changes</button>
+                    </div>
+                    </div>
+                </div>
+            </div>        
+        </form>
+
+        <!-- Modal Delete-->
+        <div wire:ignore.self class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header bg-red-400">
+                <h5 class="modal-title" id="deleteModalLabel">Konfirmasi Delete</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Apakah Anda Yakin Akan menghapus Data Ini!!
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger" wire:click.prevent="delete()" data-dismiss="modal">Ya Saya Yakin</button>
+            </div>
+            </div>
+        </div>
+        </div>
+  
 </div>
+
+
+                    
+                
+
+
